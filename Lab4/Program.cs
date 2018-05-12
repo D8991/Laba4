98670ded850ab9d1d95747c331d4ab86bcb489a0
D8991 <darkov.1998@mail.ru>
2018-05-12 12:23:06 +0300

    Первая работающая версия программы

---
+++ Lab4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Good> ListGood = new List<Good>();
+            Sale book_Sale = new Sale("Book");
+            Sale journal_Sale = new Sale("Journal");
+            Sale notebook_Sale = new Sale("NoteBook");
+
+            ListGood.Add(new Book(book_Sale, "Мастер и маргарита", 250, "Булгаков", "Роман", "Эксмо"));
+            ListGood.Add(new Good(journal_Sale, "Вестник", 150));
+            ListGood.Add(new Good(notebook_Sale, "Цветы", 60));
+            ListGood.Add(new Book(book_Sale, "Преступление и наказание", 300, "Достоевский", "Роман", "Эксмо"));
+            ListGood.Add(new Good(journal_Sale, "Наука", 160));
+
+            foreach (Good g in ListGood)
+            {
+                if (g is Book)
+                {
+                    Book b = g as Book;
+                    Console.WriteLine("Наименование товара: {0}\n Заголовок: {1}\n Цена: {2}\nАвтор: {3}\nЖанр: {4}\nИздательство: {5}", g.Good_Name, g.Book_Title, g.Price, b.Book_Author, b.Book_Genre, b.Book_Publisher);
+                }
+                else Console.WriteLine("Наименование товара: {0}\nЗаголовок:{1} \nЦена:{2}\n", g.Good_Name, g.Book_Title, g.Price);
+            }
+            Console.ReadLine();
+        }
+    }
+}
